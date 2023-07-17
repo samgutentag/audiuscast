4818e93ad64d526839499f519d0b5a03b545d4cd
Bryce <bryce@brycedev.com>
2023-07-17 18:45:09 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/home.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/home.js",
+                "resources/js/app.jsx",
+            ],
             refresh: true,
         }),
         react(),
