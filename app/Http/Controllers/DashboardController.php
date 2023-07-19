53c075da3d7b54b8e62a357604dc334ba3672dde
Bryce <bryce@brycedev.com>
2023-07-19 19:36:42 -0400

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,8 +10,10 @@ class DashboardController extends Controller
     public function index()
     {
         $podcast = Podcast::where('user_id', auth()->user()->id)->first();
-        $syncs = $podcast->syncs()->orderBy('created_at', 'desc')->get();
-
+        $syncs = [];
+        if($podcast) {
+            $syncs = $podcast->syncs()->orderBy('created_at', 'desc')->get();
+        }
         return Inertia::render('Dashboard')->with([
             'auth' => [
                 'user' => auth()->user()
