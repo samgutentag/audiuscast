48582336971c84d2db432ba7e0d0935e67dcf81f
Bryce <bryce@brycedev.com>
2023-07-31 12:08:45 -0400

    wire up env

--- audius.ts
+++ audius.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import {
     AppAuth,
     DiscoveryNodeSelector,
@@ -6,68 +9,21 @@ import {
     UploadTrackRequest,
     sdk,
     stagingConfig,
+    Genre,
 } from "@audius/sdk";
+import { SdkConfig } from "@audius/sdk/dist/sdk/types";
 
 import fs from "fs";
 import path from "path";
 
-enum Genre {
-    ALL = "All Genres",
-    ELECTRONIC = "Electronic",
-    ROCK = "Rock",
-    METAL = "Metal",
-    ALTERNATIVE = "Alternative",
-    HIP_HOP_RAP = "Hip-Hop/Rap",
-    EXPERIMENTAL = "Experimental",
-    PUNK = "Punk",
-    FOLK = "Folk",
-    POP = "Pop",
-    AMBIENT = "Ambient",
-    SOUNDTRACK = "Soundtrack",
-    WORLD = "World",
-    JAZZ = "Jazz",
-    ACOUSTIC = "Acoustic",
-    FUNK = "Funk",
-    R_AND_B_SOUL = "R&B/Soul",
-    DEVOTIONAL = "Devotional",
-    CLASSICAL = "Classical",
-    REGGAE = "Reggae",
-    PODCASTS = "Podcasts",
-    COUNTRY = "Country",
-    SPOKEN_WORK = "Spoken Word",
-    COMEDY = "Comedy",
-    BLUES = "Blues",
-    KIDS = "Kids",
-    AUDIOBOOKS = "Audiobooks",
-    LATIN = "Latin",
-    LOFI = "Lo-Fi",
-    HYPERPOP = "Hyperpop",
-    TECHNO = "Techno",
-    TRAP = "Trap",
-    HOUSE = "House",
-    TECH_HOUSE = "Tech House",
-    DEEP_HOUSE = "Deep House",
-    DISCO = "Disco",
-    ELECTRO = "Electro",
-    JUNGLE = "Jungle",
-    PROGRESSIVE_HOUSE = "Progressive House",
-    HARDSTYLE = "Hardstyle",
-    GLITCH_HOP = "Glitch Hop",
-    TRANCE = "Trance",
-    FUTURE_BASS = "Future Bass",
-    FUTURE_HOUSE = "Future House",
-    TROPICAL_HOUSE = "Tropical House",
-    DOWNTEMPO = "Downtempo",
-    DRUM_AND_BASS = "Drum & Bass",
-    DUBSTEP = "Dubstep",
-    JERSEY_CLUB = "Jersey Club",
-    VAPORWAVE = "Vaporwave",
-    MOOMBAHTON = "Moombahton",
-}
+const apiKey = process.env.AUDIUS_API_KEY || "";
+const apiSecret = process.env.AUDIUS_API_SECRET || "";
+const audiusEnv = process.env.AUDIUS_ENV || "staging";
 
-const apiKey = "b6fd9c0350e052d7af5b9d2895c6481554d57999";
-const apiSecret =
-    "9fe73bd3d003d1af23a72b8ffa32709175da78c40987b657833372822c8b3902";
+let appName = "AudiusCast_Staging";
+if (audiusEnv === "production") {
+    appName = "AudiusCast";
+}
 
 const userId = process.argv[3];
 const data = JSON.parse(process.argv[5]);
@@ -78,31 +34,41 @@ const discoveryNodeSelector = new DiscoveryNodeSelector({
     initialSelectedNode: "https://discoveryprovider.staging.audius.co/",
 });
 
-const audiusSdk = sdk({
-    appName: "AudiusCast_Staging",
-    services: {
-        discoveryNodeSelector,
-        entityManager: new EntityManager({
-            discoveryNodeSelector,
-            web3ProviderUrl: stagingConfig.web3ProviderUrl,
-            contractAddress: stagingConfig.entityManagerContractAddress,
-            identityServiceUrl: stagingConfig.identityServiceUrl,
-        }),
-        storageNodeSelector: new StorageNodeSelector({
-            discoveryNodeSelector,
-            auth: new AppAuth(apiKey, apiSecret),
-            bootstrapNodes: [
-                {
-                    delegateOwnerWallet:
-                        "0x671ddce7B4E676C9467F87e4031a917b5D6f75F0",
-                    endpoint: "https://usermetadata.staging.audius.co",
-                },
-            ],
-        }),
-    },
+const stagingEntityManager = new EntityManager({
+    discoveryNodeSelector,
+    web3ProviderUrl: stagingConfig.web3ProviderUrl,
+    contractAddress: stagingConfig.entityManagerContractAddress,
+    identityServiceUrl: stagingConfig.identityServiceUrl,
+});
+
+const stagingStorageNodeSelector = new StorageNodeSelector({
+    discoveryNodeSelector,
+    auth: new AppAuth(apiKey, apiSecret),
+    bootstrapNodes: [
+        {
+            delegateOwnerWallet: "0x671ddce7B4E676C9467F87e4031a917b5D6f75F0",
+            endpoint: "https://usermetadata.staging.audius.co",
+        },
+    ],
+});
+
+const stagingServices = {
+    discoveryNodeSelector,
+    entityManager: stagingEntityManager,
+    storageNodeSelector: stagingStorageNodeSelector,
+};
+
+const sdkConfig: SdkConfig = {
+    appName: appName,
     apiKey: apiKey,
     apiSecret: apiSecret,
-});
+};
+
+if (audiusEnv === "staging") {
+    sdkConfig.services = stagingServices;
+}
+
+const audiusSdk = sdk(sdkConfig);
 
 try {
     const uploadTrackRequest: UploadTrackRequest = {
@@ -117,7 +83,7 @@ try {
             description: data.description
                 ? data.description.substring(0, 1000)
                 : "",
-            title: data.title,
+            title: data.title || "",
             genre: Genre.PODCASTS,
             // tags: data.tags,
             releaseDate: new Date(data.date),
